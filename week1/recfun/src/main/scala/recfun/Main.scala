c7b503956974c087b0db2e34bd82895929b0733a
Anton Novopashin <antonevane@gmail.com>
2016-06-07 20:46:41 -0400

    Pascal's Triangle and Parentheses Balancing solved

--- week1/recfun/src/main/scala/recfun/Main.scala
+++ week1/recfun/src/main/scala/recfun/Main.scala
@@ -8,17 +8,43 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+    
+    println("Balance")
+    val test1 = "(if (zero? x) max (/ 1 x))"
+    val testFail = "())("
+    val testFail2 = ":-)"
+    val test2 = "()"
+    println("Result is " + balance(testFail2.toList)) 
   }
 
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
-  
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
+
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def processListOfChars(data: List[Char], par: List[Char]): Boolean = {
+      if (data.isEmpty) {
+        par.isEmpty
+      } else {
+        if (data.head == '(') {
+          processListOfChars(data.tail, par.::(data.head))
+        } else if (data.head == ')') {
+          if (par.isEmpty) false
+          else processListOfChars(data.tail, par.tail)
+        } else
+          processListOfChars(data.tail, par)
+      }
+    }
+
+    processListOfChars(chars, List())
+  }
   
   /**
    * Exercise 3
