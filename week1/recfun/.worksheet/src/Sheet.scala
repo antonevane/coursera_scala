7b1bb693f2c3b5aebe0169accf99d35b62600c46
antonevane <antonevane@gmail.com>
2016-06-07 22:09:20 -0400

    Coin change problem solved

---
+++ week1/recfun/.worksheet/src/Sheet.scala
@@ -0,0 +1,11 @@
+object Sheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(58); 
+  println("Welcome to the Scala worksheet");$skip(11); val res$0 = 
+  
+  300/5;System.out.println("""res0: Int(60) = """ + $show(res$0));$skip(9); val res$1 = 
+  300/10;System.out.println("""res1: Int(30) = """ + $show(res$1));$skip(9); val res$2 = 
+  300/20;System.out.println("""res2: Int(15) = """ + $show(res$2));$skip(9); val res$3 = 
+  300/50;System.out.println("""res3: Int(6) = """ + $show(res$3));$skip(10); val res$4 = 
+  300/100;System.out.println("""res4: Int(3) = """ + $show(res$4));$skip(10); val res$5 = 
+  300/200;System.out.println("""res5: Int(1) = """ + $show(res$5));$skip(10); val res$6 = 
+  300/500;System.out.println("""res6: Int(0) = """ + $show(res$6))}
+}
--- week1/recfun/src/main/scala/recfun/Main.scala
+++ week1/recfun/src/main/scala/recfun/Main.scala
@@ -15,6 +15,8 @@ object Main {
     val testFail2 = ":-)"
     val test2 = "()"
     println("Result is " + balance(testFail2.toList)) 
+    
+    println("Result is " +countChange(300,List(5,10,20,50,100,200,500)))
   }
 
   /**
@@ -45,9 +47,19 @@ object Main {
 
     processListOfChars(chars, List())
   }
-  
+
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def countSolutions(money: Int, coins: List[Int]): Int = {
+      if (money == 0) 1
+      else if (money < 0) 0
+      else if (coins.isEmpty && money >= 1) 0
+      else countSolutions(money, coins.tail) + countSolutions(money - coins.head, coins)
+    }
+
+    countSolutions(money, coins)
+
   }
+}
