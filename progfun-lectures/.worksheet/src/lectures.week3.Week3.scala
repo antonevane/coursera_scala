dc70b99fa7553a96249e2f4c04f994255bb8d075
Anton Novopashin <antonevane@gmail.com>
2016-06-13 12:00:40 -0400

    Lectures code moved to the own project

---
+++ progfun-lectures/.worksheet/src/lectures.week3.Week3.scala
@@ -0,0 +1,5 @@
+package lectures.week3
+
+object Week3 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(82); 
+  println("Welcome to the Scala worksheet")}
+}
---
+++ progfun-lectures/src/lectures/week3/Week3.sc
@@ -0,0 +1,5 @@
+package lectures.week3
+
+object Week3 {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+}
